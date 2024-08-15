7d130ffe506c82cb7908625b5d53c5d660774425
zeitun <zeytunmahadi718@gmail.com>
2024-08-15 11:30:43 +0300

    using OPP

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,20 @@
-#!/usr/bin/env python3
+# book.py
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            return
+        self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  
+        self.size = size  
+        self.condition = "Used"  
+
+    
+    def size(self):
+        return self._size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            return
+        self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
